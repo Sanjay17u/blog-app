78471b42f5f24e9b95a9392af90bb8e8c8cb2cf8
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-26 18:29:50 +0530

    blogModel is created.

--- server/models/blogModel.js
+++ server/models/blogModel.js
@@ -1,2 +1,26 @@
 const mongoose = require('mongoose')
 
+
+const blogSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        require: [true, "Title is required"],
+    },
+    
+    discription:{
+        type: String,
+        require: [true, "Discription is required"],
+    },
+    
+    image:{
+        type: String,
+        require: [true, "Image is required"],
+    },
+    
+
+}, {timestamps: true})
+
+
+const blogModel = mongoose.model('Blog', blogSchema)
+
+module.exports = blogModel
