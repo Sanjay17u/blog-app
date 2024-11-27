a7acde8e2fd7ff2bec88aa9e28363130b08fef00
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-27 16:59:11 +0530

    fixing UI

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -6,13 +6,14 @@ import { useSelector } from 'react-redux'
 import { Sheet, SheetClose, SheetContent, SheetDescription, SheetFooter, SheetHeader, SheetTitle, SheetTrigger } from './ui/sheet'
 import { Input } from './ui/input'
 import { Label } from './ui/label'
+import { Menu } from "lucide-react";
 
 const NavBar = () => {
   const isLogin = useSelector(state => state.isLogin);
   console.log(isLogin);
 
   return (
-    <nav className="bg-gray-900 p-4">
+    <nav className="bg-zinc-900 p-4">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
         {/* Logo Section */}
         <div className="text-white font-bold text-base flex items-center space-x-9">
@@ -48,7 +49,7 @@ const NavBar = () => {
               Logout
             </Button>
           </div>
-        )}
+         )}
 
         {/* Mobile NavBar (Sheet) */}
         <MobileNavBar />
@@ -63,15 +64,15 @@ const MobileNavBar = () => {
   return (
     <Sheet>
       <SheetTrigger asChild>
-        <Button variant="outline" className="text-white md:hidden">
-          Open Menu
+        <Button size={'icon'} variant="outline" className="text-white md:hidden rounded-full bg-gray-900">
+          <Menu/>
         </Button>
       </SheetTrigger>
 
-      <SheetContent className="bg-gray-800 text-white">
-        <SheetHeader>
-          <SheetTitle className="text-xl font-semibold">Blog-App Menu</SheetTitle>
-          <SheetDescription className="mt-2 text-sm">Navigate to your favorite sections</SheetDescription>
+      <SheetContent className="bg-zinc-900 text-white flex flex-col">
+        <SheetHeader className="flex flex-row items-center justify-between mt-2">
+          <SheetTitle className="text-sm font-bold text-white">Blog-App</SheetTitle>
+          <SheetDescription className="mt-2 text-sm"></SheetDescription>
         </SheetHeader>
 
         {/* Mobile Menu Links */}
@@ -91,7 +92,7 @@ const MobileNavBar = () => {
               <Link to="/register" className="block text-lg font-medium hover:text-blue-400 transition duration-300">Register</Link>
             </>
           ) : (
-            <Button className="block w-full bg-gray-800 text-white px-5 py-2 rounded-md hover:bg-gray-700 transition duration-300">
+            <Button className="block w-full bg-gray-700 text-white px-5 py-2 rounded-md hover:bg-gray-800 transition duration-300">
               Logout
             </Button>
           )}
@@ -99,7 +100,7 @@ const MobileNavBar = () => {
 
         <SheetFooter>
           <SheetClose asChild>
-            <Button className="bg-blue-500 text-white px-5 py-2 rounded-md hover:bg-blue-700 transition duration-300">
+            <Button className="bg-gray-700 w-full text-white px-5 py-2 rounded-md hover:bg-gray-800 transition duration-300">
               Close Menu
             </Button>
           </SheetClose>
