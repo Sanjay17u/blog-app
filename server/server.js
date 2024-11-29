b1cc5cf4c170d41208d69c18c1c36032ec0f6cf7
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-29 11:46:29 +0530

    Production Build Ready.

--- server/server.js
+++ server/server.js
@@ -2,22 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const dotenv = require('dotenv')
-const connectDB = require('./config/connectDB');
-const cookieParser = require('cookie-parser');
-const path = require('path');
-
+const connectDB = require('./config/connectDB')
+const cookieParser = require('cookie-parser')
+const path = require('path')
 
 // env config
-dotenv.config();
-
+dotenv.config()
 
 // mongoDB connection
 connectDB()
 
 // rest object
 const app = express()
-
-const DIRNAME = __dirname;
 
 // middlewares
 app.use(cors({
@@ -30,20 +26,22 @@ app.use(cookieParser())
 app.use(morgan('dev'))
 
 // Routes
-const apiRoutes = require('./routes/apiRoutes');
-const blogRoutes = require('./routes/blogRouts');
+const apiRoutes = require('./routes/apiRoutes')
+const blogRoutes = require('./routes/blogRouts')
 app.use('/api/v1/user', apiRoutes)
 app.use('/api/v1/blog', blogRoutes)
 
-app.use(express.static(path.join(DIRNAME, "..", "client", "dist")));
-app.use("*",(_,res) => {
-    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
+// Adjust the static file path
+const clientPath = path.resolve(__dirname, '..', 'client', 'dist')  // Going one level up to the root folder
+
+app.use(express.static(clientPath))
+
+app.get('*', (_, res) => {
+    res.sendFile(path.resolve(clientPath, 'index.html'))
 })
 
 // Start Server
 const PORT = process.env.PORT || 5000
-// Listen
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
-
