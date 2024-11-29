adf0660dfb60b062afec435dcdd6d5bea2b81e56
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-29 07:35:10 +0530

    Vercel Config

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,9 @@ import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
+  define: {
+    'process.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL)
+  },
   esbuild: {
     target: 'esnext', 
   },
