c9460b7761b562e770b1850611da887944fe62cc
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-29 10:41:42 +0530

    Production Build Ready.

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const dotenv = require('dotenv')
 const connectDB = require('./config/connectDB');
 const cookieParser = require('cookie-parser');
-
+const path = require('path');
 
 
 // env config
@@ -16,6 +16,8 @@ connectDB()
 
 // rest object
 const app = express()
+
+const DIRNAME = path.resolve()
 
 // middlewares
 app.use(cors({
@@ -33,8 +35,13 @@ const blogRoutes = require('./routes/blogRouts');
 app.use('/api/v1/user', apiRoutes)
 app.use('/api/v1/blog', blogRoutes)
 
+app.use(express.static(path.join(DIRNAME, "/client/dist")));
+app.use("*",(_,res) => {
+    res.sendFile(path.resolve(DIRNAME, "client","dist","index.html"));
+})
+
 // Start Server
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 // Listen
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
