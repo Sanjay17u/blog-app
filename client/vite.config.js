7699f2c30e548b1bb3f1a23b346b7d92a0acded1
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2024-11-29 06:49:50 +0530

    Config for Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,15 +2,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
+import tailwindcss from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    target: 'esnext', 
+  },
   plugins: [react()],
   css: {
     postcss: {
       plugins: [
-        require('tailwindcss'), 
-        require('autoprefixer')
+        tailwindcss, 
+        autoprefixer
       ],
     },
   },
